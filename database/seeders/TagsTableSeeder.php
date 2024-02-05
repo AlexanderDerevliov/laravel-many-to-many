c98ae51760de96e7616ecd9dafafef9a6948683c
AlexanderDerevliov <alex.feelred@gmail.com>
2024-02-05 08:42:42 +0100

    ADD: seeder and created table

---
+++ database/seeders/TagsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $tags = ['html', 'css', 'laravel', 'js', 'php'];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag);
+            $newTag->hex_color = $faker->hexColor();
+            $newTag->save();
+        }
+    }
+}
